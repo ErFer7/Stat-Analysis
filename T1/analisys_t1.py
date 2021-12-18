0fbcd795dec41d636253392321afb555a9ef641d
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2021-12-18 14:23:57 -0300

    upload

---
+++ T1/analisys_t1.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+
+'''
+Trabalho 1 de estatística.
+
+Este programa segue um paradigma funcional.
+
+Python 3.10.
+'''
+
+import os
+import stat_analysis
+
+print("INE5405 - Trabalho 1 de probabilidade e estatística")
+
+total_values = []
+sc_values = []
+rs_values = []
+nac_values = []
+outr_values = []
+metro_values = []
+int_values = []
+
+while True:
+
+    command = input(">>> ")
+
+    match command:
+        case "exit" | 'q':
+
+            print("Saindo.")
+            break
+        case "csv2json" | "c2j":
+
+            print("Convertendo de csv para json.")
+
+            stat_analysis.csv2json(os.path.join("Data", "Raw", "T1 - Coleta.csv"),
+                                   os.path.join("Data", "Processed", "T1 - Coleta.json"))
+
+            print("Concluído.")
+        case "process_data":
+            stat_analysis.process_data(os.path.join("Data", "Processed", "T1 - Coleta.json"),
+                                       os.path.join("Data", "Processed", "Total.txt"),
+                                       os.path.join("Data", "Processed", "SC.txt"),
+                                       os.path.join("Data", "Processed", "RS.txt"),
+                                       os.path.join("Data", "Processed", "NAC.txt"),
+                                       os.path.join("Data", "Processed", "OUTR.txt"),
+                                       os.path.join("Data", "Processed", "METRO.txt"),
+                                       os.path.join("Data", "Processed", "INT.txt"))
+        case "serialize_comp" | "sc":
+
+            print("Serializando dados.")
+
+            try:
+
+                with open(os.path.join("Data", "Processed", "Total.txt"), 'r', encoding="utf-8") as file:
+                    total_values = list(map(lambda val: float(val.strip()), file.readlines()))
+
+                print("Total serizalizado.")
+
+                with open(os.path.join("Data", "Processed", "SC.txt"), 'r', encoding="utf-8") as file:
+                    sc_values = list(map(lambda val: float(val.strip()), file.readlines()))
+
+                print("SC serizalizado.")
+
+                with open(os.path.join("Data", "Processed", "RS.txt"), 'r', encoding="utf-8") as file:
+                    rs_values = list(map(lambda val: float(val.strip()), file.readlines()))
+
+                print("RS serizalizado.")
+
+                with open(os.path.join("Data", "Processed", "NAC.txt"), 'r', encoding="utf-8") as file:
+                    nac_values = list(map(lambda val: float(val.strip()), file.readlines()))
+
+                print("NAC serizalizado.")
+
+                with open(os.path.join("Data", "Processed", "OUTR.txt"), 'r', encoding="utf-8") as file:
+                    outr_values = list(map(lambda val: float(val.strip()), file.readlines()))
+
+                print("OUTR serizalizado.")
+
+                with open(os.path.join("Data", "Processed", "METRO.txt"), 'r', encoding="utf-8") as file:
+                    metro_values = list(map(lambda val: float(val.strip()), file.readlines()))
+
+                print("METRO serizalizado.")
+
+                with open(os.path.join("Data", "Processed", "INT.txt"), 'r', encoding="utf-8") as file:
+                    int_values = list(map(lambda val: float(val.strip()), file.readlines()))
+
+                print("INT serizalizado.")
+            except FileNotFoundError:
+
+                print("Os arquivos não existem, lembre-se de processar os dados!")
+
+            print("Concluído.")
+        case "stem_and_leaves" | "sl":
+
+            print("Construindo diagramas de ramos e folhas.")
+
+            stat_analysis.stem_and_leaves(total_values,
+                                          sc_values,
+                                          rs_values,
+                                          "Santa Catarina",
+                                          "Rio Grande do Sul",
+                                          os.path.join("Data", "Represented", "Stem and Leaves - State.txt"))
+
+            print("Diagrama de estados construído.")
+
+            stat_analysis.stem_and_leaves(total_values,
+                                          nac_values,
+                                          outr_values,
+                                          "Nacionais",
+                                          "Outros",
+                                          os.path.join("Data", "Represented", "Stem and Leaves - Gas Station.txt"))
+
+            print("Diagrama de postos construído.")
+
+            stat_analysis.stem_and_leaves(total_values,
+                                          metro_values,
+                                          int_values,
+                                          "Metropolitanas",
+                                          "Interioranas",
+                                          os.path.join("Data", "Represented", "Stem and Leaves - Region.txt"))
+
+            print("Diagrama de regiões construído.")
+
+            print("Concluído.")
+        case "empirical_model" | "em":
+
+            print("Construindo modelos empíricos.")
+
+            stat_analysis.empirical_model(total_values,
+                                          os.path.join("Data", "Represented", "Empirical Model - Total.txt"))
+
+            print("Total construído.")
+
+            stat_analysis.empirical_model(sc_values,
+                                          os.path.join("Data", "Represented", "Empirical Model - SC.txt"))
+
+            print("SC construído.")
+
+            stat_analysis.empirical_model(rs_values,
+                                          os.path.join("Data", "Represented", "Empirical Model - RS.txt"))
+
+            print("RS construído.")
+
+            stat_analysis.empirical_model(nac_values,
+                                          os.path.join("Data", "Represented", "Empirical Model - NAC.txt"))
+
+            print("NAC construído.")
+
+            stat_analysis.empirical_model(outr_values,
+                                          os.path.join("Data", "Represented", "Empirical Model - OUTR.txt"))
+
+            print("OUTR construído.")
+
+            stat_analysis.empirical_model(metro_values,
+                                          os.path.join("Data", "Represented", "Empirical Model - METRO.txt"))
+
+            print("METRO construído.")
+
+            stat_analysis.empirical_model(int_values,
+                                          os.path.join("Data", "Represented", "Empirical Model - INT.txt"))
+
+            print("INT construído.")
+
+            print("Concluído.")
+        case "histogram" | "h":
+
+            print("Construindo histogramas.")
+
+            stat_analysis.histogram("Total",
+                                    "#0000CC",
+                                    total_values)
+
+            print("Total construído.")
+
+            stat_analysis.histogram("Santa Catarina",
+                                    "#0000CC",
+                                    sc_values)
+
+            print("SC construído.")
+
+            stat_analysis.histogram("Rio Grande do Sul",
+                                    "#0000CC",
+                                    rs_values)
+
+            print("RS construído.")
+
+            stat_analysis.histogram("Postos nacionais",
+                                    "#0000CC",
+                                    nac_values)
+
+            print("NAC construído.")
+
+            stat_analysis.histogram("Outros postos",
+                                    "#0000CC",
+                                    outr_values)
+
+            print("OUTR construído.")
+
+            stat_analysis.histogram("Região metropolitana",
+                                    "#0000CC",
+                                    metro_values)
+
+            print("METRO construído.")
+
+            stat_analysis.histogram("Interior",
+                                    "#0000CC",
+                                    int_values)
+
+            print("INT construído.")
+
+            print("Concluído.")
+        case "box_plot" | "bp":
+
+            print("Construindo diagramas em caixas")
+
+            stat_analysis.box_plot(sc_values, rs_values)
+
+            print("Concluído.")
+        case _:
+            print("Comando não reconhecido.")
---
+++ T1/stat_analysis.py
@@ -0,0 +1,421 @@
+# -*- coding: utf-8 -*-
+
+'''
+Análise estatística.
+'''
+
+import csv
+import json
+
+from math import log10, ceil, sqrt
+from statistics import median, mode, variance, stdev
+
+import plotly.graph_objects as go
+
+
+def csv2json(input_file_path: str, output_file_path: str):
+    '''
+    Gera um arquivo .json a partir de um arquivo csv.
+    '''
+
+    processed_dict = {}
+
+    with open(input_file_path, mode='r', encoding="utf-8") as csv_file:
+
+        csv_reader = csv.reader(csv_file)
+
+        for row in csv_reader:
+            processed_dict[row[0]] = row[1:]
+
+    with open(output_file_path, 'w+', encoding="utf-8") as file:
+
+        data_json = json.dumps(processed_dict, indent=4)
+        file.write(data_json)
+
+
+def process_data(input_file_path: str, *output_files_path: str):
+    '''
+    Processa todos os dados e gera arquivos com os dados agrupados.
+    '''
+
+    data = {}
+
+    with open(input_file_path, 'r', encoding="utf-8") as json_file:
+
+        data = json.load(json_file)
+
+    total_values = []
+    sc_values = []
+    rs_values = []
+    nac_values = []
+    outr_values = []
+    metro_values = []
+    int_values = []
+
+    for key in data:
+
+        value = float(data[key][4].replace(',', '.'))
+
+        total_values.append(value)
+
+        if data[key][1] == "SC":
+            sc_values.append(value)
+        else:
+            rs_values.append(value)
+
+        if data[key][3] == "NAC":
+            nac_values.append(value)
+        else:
+            outr_values.append(value)
+
+        if data[key][5] == "METRO":
+            metro_values.append(value)
+        else:
+            int_values.append(value)
+
+    for i in range(7):
+
+        with open(output_files_path[i], 'w+', encoding="utf-8") as file:
+
+            match i:
+
+                case 0:
+
+                    for value in total_values:
+                        file.write(str(value) + '\n')
+                case 1:
+
+                    for value in sc_values:
+                        file.write(str(value) + '\n')
+                case 2:
+
+                    for value in rs_values:
+                        file.write(str(value) + '\n')
+                case 3:
+
+                    for value in nac_values:
+                        file.write(str(value) + '\n')
+                case 4:
+
+                    for value in outr_values:
+                        file.write(str(value) + '\n')
+                case 5:
+
+                    for value in metro_values:
+                        file.write(str(value) + '\n')
+                case 6:
+
+                    for value in int_values:
+                        file.write(str(value) + '\n')
+
+
+def stem_and_leaves(total_data: list,
+                    data_left: list,
+                    data_right: list,
+                    header_left: str,
+                    header_right: str,
+                    output_file_path: str):
+    '''
+    Cria um diagram de ramos e folhas comparativo.
+    '''
+
+    center_column_left = {}
+    center_column_right = {}
+
+    total_data.sort()
+    data_left.sort()
+    data_right.sort()
+
+    for value in total_data:
+
+        val_stem_key = f"{value:.3f}"[:3]
+
+        if val_stem_key not in center_column_left:
+            center_column_left[val_stem_key] = {}
+
+        if val_stem_key not in center_column_right:
+            center_column_right[val_stem_key] = {}
+
+    for value in data_left:
+
+        val_stem_key = f"{value:.3f}"[:3]
+        val_leaf_key = f"{value:.3f}"[3:]
+
+        if val_leaf_key in center_column_left[val_stem_key]:
+            center_column_left[val_stem_key][val_leaf_key] += 1
+        else:
+            center_column_left[val_stem_key][val_leaf_key] = 1
+
+    for value in data_right:
+
+        val_stem_key = f"{value:.3f}"[:3]
+        val_leaf_key = f"{value:.3f}"[3:]
+
+        if val_leaf_key in center_column_right[val_stem_key]:
+            center_column_right[val_stem_key][val_leaf_key] += 1
+        else:
+            center_column_right[val_stem_key][val_leaf_key] = 1
+
+    stem = list(center_column_left.keys())
+
+    min_key = float(stem[0])
+    max_key = float(stem[-1])
+    current_key = min_key
+    i = 0
+
+    while current_key < max_key:
+
+        str_key = f"{current_key:.1f}"
+
+        if str_key not in stem:
+            stem.insert(i, str_key)
+
+        i += 1
+        current_key += 0.1
+
+    left_leaves = []
+    right_leaves = []
+    left_frequency = []
+    right_frequency = []
+
+    for i, key in enumerate(stem):
+        left_leaves.append([])
+        right_leaves.append([])
+
+        left_sum = 0
+
+        if key in center_column_left:
+
+            for leaf in center_column_left[key]:
+                left_sum += center_column_left[key][leaf]
+                left_leaves[i].append((leaf, center_column_left[key][leaf]))
+
+        right_sum = 0
+
+        if key in center_column_right:
+
+            for leaf in center_column_right[key]:
+                right_sum += center_column_right[key][leaf]
+                right_leaves[i].append((leaf, center_column_right[key][leaf]))
+
+        left_frequency.append(left_sum)
+        right_frequency.append(right_sum)
+        left_leaves[i] = sorted(left_leaves[i], key=lambda val: float(val[0]), reverse=True)
+        right_leaves[i] = sorted(right_leaves[i], key=lambda val: float(val[0]))
+
+    for i, branch in enumerate(left_leaves):
+
+        str_branch = ''
+
+        for leaf in branch:
+            str_branch += f"({leaf[0]}/{leaf[1]}) "
+
+        left_leaves[i] = str_branch
+
+    for i, branch in enumerate(right_leaves):
+
+        str_branch = ''
+
+        for leaf in branch:
+            str_branch += f"({leaf[0]}/{leaf[1]}) "
+
+        right_leaves[i] = str_branch
+
+    with open(output_file_path, 'w+', encoding="utf-8") as file:
+
+        file.write(f"Freq:Folhas ({header_left}):Ramos:Folhas ({header_right}):Freq\n")
+
+        for i, branch in enumerate(stem):
+            file.write(f"{left_frequency[i]}:{left_leaves[i]}:{branch}:{right_leaves[i]}:{right_frequency[i]}\n")
+
+
+def generate_classes(data: list):
+    '''
+    Gera classes de um modelo empírico.
+    '''
+
+    range_ = max(data) - min(data)
+    k = 1 + 3.32 * log10(len(data))
+    class_amp = range_ / k
+
+    print("Geração de classes.")
+    print(f"Mínimo: {min(data):.3f}")
+    print(f"Máximo: {max(data):.3f}")
+    print(f"Range: {range_:.3f}")
+    print(f"k: {k:.3f} ou {ceil(k)}")
+    print(f"Amplitude: {class_amp:.3f}")
+
+    k = ceil(k)
+
+    classes = []
+
+    for i in range(k):
+        classes.append((min(data) + class_amp * i, min(data) + class_amp * (i + 1)))
+
+    return classes
+
+
+def frequency_count(classes: list, data: list):
+    '''
+    Gera uma lista com a contagem pelas classes.
+    '''
+
+    frequency = [0 for _ in range(len(classes))]
+
+    for value in data:
+
+        for i, class_ in enumerate(classes):
+
+            if class_[1] > value >= class_[0]:
+                frequency[i] += 1
+
+    return frequency
+
+
+def empirical_model(data: list, output_file_path: str):
+    '''
+    Gera uma tabela do modelo empírico.
+    '''
+
+    classes = generate_classes(data)
+    frequency = frequency_count(classes, data)
+    proportion = []
+    acc_proportion = []
+    middle_point = []
+    weighted_average = []
+    variance_ = []
+
+    for freq in frequency:
+        proportion.append(freq / len(data))
+
+    acc_prop = 0.0
+
+    for prop in proportion:
+
+        acc_proportion.append(prop + acc_prop)
+        acc_prop += prop
+
+    for i, class_ in enumerate(classes):
+
+        middle_point.append((class_[0] + class_[1]) / 2)
+        weighted_average.append(proportion[i] * middle_point[i])
+
+    weighted_average_sum = sum(weighted_average)
+
+    for i, class_ in enumerate(classes):
+        variance_.append(((middle_point[i] - weighted_average_sum) ** 2) * proportion[i])
+
+    median_ = median(data)
+    mode_ = middle_point[frequency.index(max(frequency))]
+    variance_sum = sum(variance_)
+    std_deviation = sqrt(variance_sum)
+    std_avg_error = std_deviation / sqrt(len(data))
+    variation_coeff = std_deviation / weighted_average_sum
+    median_assimetry = 3 * (weighted_average_sum - median_) / std_deviation
+    mode_assimetry = (weighted_average_sum - mode_) / std_deviation
+
+    simple_average = sum(data) / len(data)
+    median_nda = median(data)
+    mode_nda = mode(data)
+    variance_nda = variance(data)
+    std_deviation_nda = stdev(data)
+    std_avg_error_nda = std_deviation_nda / sqrt(len(data))
+    variation_coeff_nda = std_deviation_nda / simple_average
+    median_assimetry_nda = 3 * (simple_average - median_nda) / std_deviation_nda
+    mode_assimetry_nda = (simple_average - mode_nda) / std_deviation_nda
+
+    relative_error = (abs(weighted_average_sum - sum(data) / len(data)) / weighted_average_sum)
+
+    with open(output_file_path, 'w+', encoding="utf-8") as file:
+
+        file.write("Classe:Frequência:pi:Pi:Xi:Xi * pi:di^2 * pi\n")
+
+        for i, class_ in enumerate(classes):
+
+            str_class = f"{class_[0]:.3f} ˫ {class_[1]:.3f}:{frequency[i]}:{proportion[i]:.3f}:" + \
+                        f"{acc_proportion[i]:.3f}:{middle_point[i]:.3f}:{weighted_average[i]:.3f}:{variance_[i]:.3f}\n"
+
+            str_class = str_class.replace('.', ',')
+            file.write(str_class)
+
+        str_total = f"Total:{sum(frequency)}:{sum(proportion):.3f}:-:-:{weighted_average_sum:.3f}:" + \
+                    f"{variance_sum:.3f}\n\n"
+
+        str_total = str_total.replace('.', ',')
+        file.write(str_total)
+
+        file.write(f"Média ponderada:{weighted_average_sum:.3f}:".replace('.', ',') + \
+                   f"Média simples:{simple_average:.3f}\n".replace('.', ','))
+
+        file.write(f"Mediana:{median_:.3f}:".replace('.', ',') + \
+                   f"Mediana NDA:{median_nda:.3f}\n".replace('.', ','))
+
+        file.write(f"Moda:{mode_:.3f}:".replace('.', ',') + \
+                   f"Moda NDA:{mode_nda:.3f}\n".replace('.', ','))
+
+        file.write(f"Variância:{variance_sum:.3f}:".replace('.', ',') + \
+                   f"Variância NDA:{variance_nda:.3f}\n".replace('.', ','))
+
+        file.write(f"Desvio Padrão:{std_deviation:.3f}:".replace('.', ',') + \
+                   f"Desvio Padrão NDA:{std_deviation_nda:.3f}\n".replace('.', ','))
+
+        file.write(f"Erro padrão da média:{std_avg_error:.3f}:".replace('.', ',') + \
+                   f"Erro padrão da média NDA:{std_avg_error_nda:.3f}\n".replace('.', ','))
+
+        file.write(f"Coeficiente de variação:{variation_coeff:.3f}:".replace('.', ',') + \
+                   f"Coeficiente de variação NDA:{variation_coeff_nda:.3f}\n".replace('.', ','))
+
+        file.write(f"Assimetria da mediana:{median_assimetry:.3f}:".replace('.', ',') + \
+                   f"Assimetria da mediana NDA:{median_assimetry_nda:.3f}\n".replace('.', ','))
+
+        file.write(f"Assimetria da moda:{mode_assimetry:.3f}:".replace('.', ',') + \
+                   f"Assimetria da moda NDA:{mode_assimetry_nda:.3f}\n".replace('.', ','))
+
+        file.write(f"Erro relativo:{relative_error:.5f}\n".replace('.', ','))
+
+
+def histogram(title: str, color: str, data: list):
+    '''
+    Gera um histograma com o dados comparativos.
+    '''
+
+    classes = generate_classes(data)
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Histogram(
+        x=data,
+        xbins=dict(
+            start=classes[0][0],
+            end=classes[-1][1],
+            size=(classes[1][0] + classes[1][1]) / 2 - (classes[0][0] + classes[0][1]) / 2
+        ),
+        marker_color=color,
+        opacity=0.75
+    ))
+
+    fig.update_layout(
+        title_text=title,
+        xaxis=dict(
+            ticks="outside",
+            tick0=(classes[0][0] + classes[0][1]) / 2,
+            dtick=(classes[1][0] + classes[1][1]) / 2 - (classes[0][0] + classes[0][1]) / 2),
+        xaxis_title_text='Pontos médio da classe',
+        yaxis_title_text='Frequência',
+        bargap=0.1,
+        bargroupgap=0
+    )
+
+    fig.show()
+
+
+def box_plot(data_a: list, data_b: list):
+    '''
+    Faz o plot de um diagrama de caixas comparativo.
+    '''
+
+    fig = go.Figure()
+    fig.add_trace(go.Box(y=data_a))
+    fig.add_trace(go.Box(y=data_b))
+
+    fig.show()
